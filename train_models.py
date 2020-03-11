51c93d7d91dd14c358f3e43ca85297c4a2b20a0d
Eric B Munson <ebm@cs.toronto.edu>
2020-03-11 08:09:11 -0400

    Add new training method that spans entire corpus
    Instead of training windows, tag each token with the congress where it
    was found and train one model with all input.
    
    Signed-off-by: Eric B Munson <ebm@cs.toronto.edu>

--- train_models.py
+++ train_models.py
@@ -7,7 +7,7 @@ import argparse
 from subprocess import Popen, PIPE
 from gensim.scripts.glove2word2vec import glove2word2vec
 
-def read_input_data(windowing, target, training):
+def read_windowed_input_data(windowing, target, training):
     if windowing == 'middle':
         p1 = '{}/prepared_{:03d}.txt'.format(training, target - 1)
         p2 = '{}/prepared_{:03d}.txt'.format(training, target)
@@ -24,24 +24,64 @@ def read_input_data(windowing, target, training):
     return ' '.join(input)
 
 
-def build_complete_vocab_file(args):
+def read_tagged_input(training, start, end):
     input = []
-    for i in range(53, 111):
-        with open('{}/prepared_{:03d}.txt'.format(args.training, i), 'r') as fd:
+    for target in range(start, end + 1):
+        with open('{}/prepared_{:03d}.txt'.format(training, target), 'r') as fd:
             for line in fd:
-                input.append(line)
+                words = [ '{}_{}'.format(word, target) for word in line.split() if not word.isspace() ]
+                input.append((' '.join(words)).strip())
+    return ' '.join(input)
 
-    cmd = ['{}/vocab_count'.format(args.glove), '-min-count', str(args.min_count), '-verbose', '2']
-    p = Popen(cmd, bufsize=1073741824, stdin=PIPE, stdout=PIPE, universal_newlines=True)
-    out,_ = p.communicate(input=' '.join(input))
-    with open('{}/vocab_complete.txt'.format(args.output), 'w') as fd:
+
+def run_vocab(input, args, target):
+    exe = "{}/vocab_count".format(args.glove)
+    cmd = [exe, '-min-count', str(args.min_count), '-verbose', '2']
+    p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE, universal_newlines=True)
+    out,_ = p.communicate(input=input)
+    vocab_file = '{}/vocab_{}_{}.txt'.format(args.output, args.windowing, target)
+    with open(vocab_file, 'w') as fd:
         fd.write(out)
+    return vocab_file
+
+
+def run_cooccur_and_shuffle(input, args, vocab_file, target):
+    exe = '{}/cooccur'.format(args.glove)
+    cmd = [exe, '-memory', str(args.memory), '-verbose', '2', '-vocab-file', vocab_file, '-window-size', '15']
+    p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE)
+    coocur,_ = p.communicate(input=bytes(input, 'utf-8'))
+
+    exe = '{}/shuffle'.format(args.glove)
+    cmd = [exe, '-memory', str(args.memory), '-verbose', '2']
+    p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE)
+    out,_ = p.communicate(input=coocur)
+    cooccur_shuff_file = '{}/cooccurrence_{}_{}.shuf.bin'.format(args.output, args.windowing, target)
+    with open(cooccur_shuff_file, 'wb') as fd:
+        fd.write(out)
+    return cooccur_shuff_file
+
+
+def run_glove(args, vocab_file, cooccur_shuff_file, target):
+    exe = '{}/glove'.format(args.glove)
+    cmd = [exe, '-save-file', '{}/vectors_{}_{:03d}'.format(args.output, args.windowing, target), '-threads',
+    str(numCpus), '-input-file', cooccur_shuff_file, '-x-max', '10', '-iter', str(args.max_iterations),
+    '-vector-size', str(args.vector_size), '-binary', '2', '-vocab-file', vocab_file, '-verbose', '2']
+    p = Popen(cmd)
+    p.communicate()
+
+    os.remove(cooccur_shuff_file)
+    os.remove(vocab_file)
+    os.remove('{}/vectors_{}_{:03d}.bin'.format(args.output, args.windowing, target))
+
+    glove2word2vec('{}/vectors_{}_{:03d}.txt'.format(args.output, args.windowing, target),
+                '{}/vectors_{}_{}.word2vec'.format(args.output, args.windowing, target))
+    os.remove('{}/vectors_{}_{:03d}.txt'.format(args.output, args.windowing, target))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run GloVe training steps  with specified configuration.")
     parser.add_argument('-t', '--training', default='prepared_txt', help='path to prepared data')
-    parser.add_argument('-w', '--windowing', default='middle', choices=['middle', 'leading'], help='How to window the training data around the target, middle is wtw and leading is wwt')
+    parser.add_argument('-w', '--windowing', default='middle', choices=['middle', 'leading', 'none'], help='How to window the training data around the target, middle is wtw and leading is wwt, none is a single model with words tagged by congress')
     parser.add_argument('-o', '--output', default='models', help='path where resulting models and intermediate files should be placed')
     parser.add_argument('-g', '--glove', default='./GloVe/build', help='Path where the GloVe executables can be found')
     parser.add_argument('-s', '--start',  default=55, type=int, help='Congress to start with')
@@ -55,44 +95,20 @@ if __name__ == "__main__":
 
     numCpus = multiprocessing.cpu_count()
 
-    #build_complete_vocab_file(args)
+    if args.windowing == 'none':
+        input = read_tagged_input(args.training, args.start, args.end)
+        vocab_file = run_vocab(input, args, 0)
 
-    for target in range(args.start, args.end + 1):
-        input = read_input_data(args.windowing, target, args.training)
-        exe = "{}/vocab_count".format(args.glove)
-        cmd = [exe, '-min-count', str(args.min_count), '-verbose', '2']
-        p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE, universal_newlines=True)
-        out,_ = p.communicate(input=input)
-        vocab_file = '{}/vocab_{}_{}.txt'.format(args.output, args.windowing, target)
-        with open(vocab_file, 'w') as fd:
-            fd.write(out)
+        cooccur_shuff_file = run_cooccur_and_shuffle(input, args, vocab_file, 0)
 
-        exe = '{}/cooccur'.format(args.glove)
-        cmd = [exe, '-memory', str(args.memory), '-verbose', '2', '-vocab-file', vocab_file, '-window-size', '15']
-        p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE)
-        coocur,_ = p.communicate(input=bytes(input, 'utf-8'))
+        run_glove(args, vocab_file, cooccur_shuff_file, 0)
 
-        exe = '{}/shuffle'.format(args.glove)
-        cmd = [exe, '-memory', str(args.memory), '-verbose', '2']
-        p = Popen(cmd, bufsize=268435456, stdin=PIPE, stdout=PIPE)
-        out,_ = p.communicate(input=coocur)
-        cooccur_shuff_file = '{}/cooccurrence_{}_{}.shuf.bin'.format(args.output, args.windowing, target)
-        with open(cooccur_shuff_file, 'wb') as fd:
-            fd.write(out)
+    else:
+        for target in range(args.start, args.end + 1):
+            input = read_windowed_input_data(args.windowing, target, args.training)
 
-        exe = '{}/glove'.format(args.glove)
-        cmd = [exe, '-save-file', '{}/vectors_{}_{:03d}'.format(args.output, args.windowing, target), '-threads',
-        str(numCpus), '-input-file', cooccur_shuff_file, '-x-max', '10', '-iter', str(args.max_iterations),
-        '-vector-size', str(args.vector_size), '-binary', '2', '-vocab-file', vocab_file, '-verbose', '2']
-        p = Popen(cmd)
-        p.communicate()
+            vocab_file = run_vocab(input, args, target)
 
-        os.remove(cooccur_shuff_file)
-        os.remove(vocab_file)
-        os.remove('{}/vectors_{}_{:03d}.bin'.format(args.output, args.windowing, target))
+            cooccur_shuff_file = run_cooccur_and_shuffle(input, args, vocab_file, target)
 
-        glove2word2vec('{}/vectors_{}_{:03d}.txt'.format(args.output, args.windowing, target),
-                       '{}/vectors_{}_{}.word2vec'.format(args.output, args.windowing, target))
-        os.remove('{}/vectors_{}_{:03d}.txt'.format(args.output, args.windowing, target))
-
-    #os.remove('{}/vocab_complete.txt'.format(args.output))
+            run_glove(args, vocab_file, cooccur_shuff_file, target)
